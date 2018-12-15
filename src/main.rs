a22511375e170aea94d1e3f20739f491a7ff8144
Oivvio Polite <oivvio@polite.se>
2018-12-15 21:07:24 +0100

    Fixed a bug that didn't handle the edge of the screen correctyl. ->
    Replaced print_field with one that takes the old field and the new and
    only prints the difference

--- src/main.rs
+++ src/main.rs
@@ -2,35 +2,30 @@ extern crate ncurses;
 
 use ncurses::*;
 
-use std::{thread, time};
-
-// extern crate rand;
-
 use rand::Rng;
 
-const NCOLS: i16 = 100;
+const NCOLS: i16 = 200;
 const NROWS: i16 = 40;
-const NCELLS: i16 = NCOLS * NROWS + 1000;
-
-// type Field = ([u8; NCELLS as usize], i16, i16);
+const NCELLS: i16 = NCOLS * NROWS;
 
 struct Field {
     data: [u8; NCELLS as usize],
-    rows: i16,
-    cols: i16,
+    nrows: i16,
+    ncols: i16,
 }
 
-fn print_field(field: &Field) {
+fn print_field_diff(old_field: &Field, new_field: &Field) {
     printw("\n");
 
-    for row in 0..field.rows {
-        for col in 0..field.cols {
-            let index = (col + (row * field.cols)) as usize;
-            let val = field.data[index];
-            match val {
-                0 => printw(" "),
-                _ => printw("X"),
-            };
+    for row in 0..old_field.nrows {
+        for col in 0..old_field.ncols {
+            let index = get_index(&old_field, row, col);
+            if old_field.data[index] != new_field.data[index] {
+                match new_field.data[index] {
+                    0 => mvprintw(row as i32, col as i32, " "),
+                    _ => mvprintw(row as i32, col as i32, "X"),
+                };
+            }
         }
         printw("\n");
     }
@@ -40,8 +35,8 @@ fn print_field(field: &Field) {
 fn get_new_field() -> Field {
     let field = Field {
         data: [0; NCELLS as usize],
-        rows: NROWS,
-        cols: NCOLS,
+        nrows: NROWS,
+        ncols: NCOLS,
     };
     return field;
 }
@@ -49,67 +44,56 @@ fn get_new_field() -> Field {
 fn get_new_random_field() -> Field {
     let mut field = Field {
         data: [0; NCELLS as usize],
-        rows: NROWS,
-        cols: NCOLS,
+        nrows: NROWS,
+        ncols: NCOLS,
     };
 
-    for row in 0..NROWS {
-        for col in 0..NCOLS {
-            let index = get_index(row, col);
+    for row in 0..field.nrows {
+        for col in 0..field.ncols {
+            let index = get_index(&field, row, col);
             field.data[index] = rand::thread_rng().gen_range(0, 2);
         }
     }
 
-    return field;
+    field
 }
 
-fn get_index(row: i16, col: i16) -> usize {
+fn get_index(field: &Field, row: i16, col: i16) -> usize {
     let mut row = row;
     let mut col = col;
 
     if row < 0 {
-        row = NROWS;
+        row = field.nrows - 1;
     }
-    if row > NROWS {
+
+    if row >= field.nrows {
         row = 0;
     }
 
     if col < 0 {
-        col = NCOLS;
+        col = field.ncols - 1;
     }
 
-    if col > NCOLS {
+    if col >= field.ncols {
         col = 0;
     }
 
-    (col + (row * NCOLS)) as usize
+    (col + (row * field.ncols)) as usize
 }
 
 fn count_neighbours(field: &Field, row: i16, col: i16) -> u8 {
     let mut count: u8 = 0;
 
-    count += field.data[get_index(row - 1, col - 1)];
-    count += field.data[get_index(row - 1, col)];
-    count += field.data[get_index(row - 1, col + 1)];
+    count += field.data[get_index(&field, row - 1, col - 1)];
+    count += field.data[get_index(&field, row - 1, col)];
+    count += field.data[get_index(&field, row - 1, col + 1)];
 
-    count += field.data[get_index(row, col - 1)];
-    count += field.data[get_index(row, col + 1)];
+    count += field.data[get_index(&field, row, col - 1)];
+    count += field.data[get_index(&field, row, col + 1)];
 
-    count += field.data[get_index(row + 1, col - 1)];
-    count += field.data[get_index(row + 1, col)];
-    count += field.data[get_index(row + 1, col + 1)];
-
-    count
-}
-
-fn count_live_cells(field: Field) -> u16 {
-    let mut count: u16 = 0;
-    for row in 0..NROWS {
-        for col in 0..NCOLS {
-            // Count neighbours at this postion
-            count += (field.data[get_index(row, col)] as u16);
-        }
-    }
+    count += field.data[get_index(&field, row + 1, col - 1)];
+    count += field.data[get_index(&field, row + 1, col)];
+    count += field.data[get_index(&field, row + 1, col + 1)];
 
     count
 }
@@ -119,12 +103,12 @@ fn update_field(field: &Field) -> Field {
     let mut new_field = get_new_field();
 
     // Look att all positions in old field
-    for row in 0..NROWS {
-        for col in 0..NCOLS {
+    for row in 0..field.nrows {
+        for col in 0..field.ncols {
             // Count neighbours at this postion
             let count = count_neighbours(&field, row, col);
-            let current_cell = field.data[get_index(row, col)];
-            new_field.data[get_index(row, col)] = match (current_cell, count) {
+            let current_cell = field.data[get_index(&field, row, col)];
+            new_field.data[get_index(&field, row, col)] = match (current_cell, count) {
                 (1, 2) | (1, 3) | (0, 3) => 1,
                 _ => 0,
             };
@@ -134,22 +118,24 @@ fn update_field(field: &Field) -> Field {
 }
 
 fn main() {
-    let ten_millis = time::Duration::from_millis(50);
+    // let ten_millis = time::Duration::from_millis(50);
 
     initscr();
     let mut field = get_new_random_field();
+    let mut new_field;
+
     let stop = 1000;
 
     for index in 1..stop {
-        field = update_field(&field);
-        print_field(&field);
-
+        new_field = update_field(&field);
+        print_field_diff(&new_field, &field);
+        field = new_field;
         if index < stop - 1 {
             clear();
         }
     }
+
     printw("Press key\n");
     getch();
-
     endwin();
 }
